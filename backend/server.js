57a906de81cd811fc0ef670dbad5e1c86f59872a
Shivam Jha <sjhaxkira@gmail.com>
2025-03-09 20:36:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,10 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/user", userRoutes)
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 
