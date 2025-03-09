6f6ce73e4d68ceef607c426df904f8a2bbe75053
Shivam Jha <sjhaxkira@gmail.com>
2025-03-09 20:49:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-proxy: {
-  "/api": {
-    target: "https://chat-application-ed5w.onrender.com",
-    changeOrigin: true,
-    secure: false,
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
-},
+});
