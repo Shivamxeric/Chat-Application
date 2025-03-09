bc3144fe754eefda66eccd9458d48447e08a3798
Shivam Jha <sjhaxkira@gmail.com>
2025-03-09 22:13:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,10 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+app.use(express.json());  // For JSON request body parsing
+app.use(express.urlencoded({ extended: true })); // Handles form data
+app.use(cookieParser());  // Handles cookies
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
-app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
